d925864b85ff17cbda615c9e6519cb3be854a72d
danargh <danargh86@gmail.com>
2024-12-09 22:06:37 +0700

    feat: add leetcode problem

---
+++ leetcode/algorithm/12-jump-game.ts
@@ -0,0 +1,23 @@
+function canJump(nums: number[]): boolean {
+   const len = nums.length;
+   let maxReach = 0; // maximum index we can reach
+
+   for (let i = 0; i < len; i++) {
+      // If the current index is not reachable from the previous indices
+      // return false
+      if (i > maxReach) {
+         return false;
+      }
+      // Update the maximum reachable index
+      maxReach = Math.max(maxReach, i + nums[i]);
+      // If we can reach the end, return true
+      if (maxReach >= len - 1) {
+         return true;
+      }
+   }
+   // We've iterated through the array and haven't reached the end,
+   // so return false
+   return false;
+}
+
+console.log(canJump([3, 2, 1, 1, 4]));
